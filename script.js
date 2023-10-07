c76030f0456aa94e428f87c7f05fcfef4217273e
Mary Imarenezor <maryimarenezor13@gmail.com>
2023-10-07 11:00:21 -0500

    patched up and bugs fixed

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-let resetBut = document.querySelector("#resetBut")
-let cells = Array.from(document.querySelectorAll(".cell"))
-let boxes = Array(9).fill(null)
-let resultText = document.querySelector(".result-text")
+window.addEventListener("DOMContentLoaded", (event) => {
+    let resetBut = document.querySelector("#resetBut");
+    let cells = Array.from(document.querySelectorAll(".cell"));
+    let boxes = Array(9).fill(null);
 
+    const iconO = "O";
+    const iconX = "X";
+    let currentPlayer = iconX;
 
-const iconO = "O"
-const iconX = "X"
-let currentPlayer = iconX
+    function handleCellClick(event) {
+        const idSelected = parseInt(event.target.id);
 
-function gameStart() {
-    cells.forEach(e => e.addEventListener("click", function(g) {
-        console.log(g.target)
-        const idSelected = g.target.id
+        console.log("idSelected:", idSelected);
+        console.log("cells[idSelected]:", cells[idSelected]);
 
-        if(!boxes[idSelected]) {
-            boxes[idSelected] = currentPlayer
-            g.target.innerText = currentPlayer
-
-            currentPlayer = currentPlayer == iconX ? iconO : iconX
+        if (!isNaN(idSelected) && !cells[idSelected].innerText) {
+            cells[idSelected].innerText = currentPlayer;
+            boxes[idSelected] = currentPlayer;
+            currentPlayer = currentPlayer == iconX ? iconO : iconX;
         }
-    }))
-}
 
-resetBut.addEventListener("click", function() {
-    boxes.full(null)
 
-    cells.forEach(e => {
-        e.innerText = ""
-    })
+    }
 
-    currentPlayer = iconX
-})
+    cells.forEach((cell) => cell.addEventListener("click", handleCellClick));
 
-gameStart()
+    resetBut.addEventListener("click", function () {
+        boxes.fill(null);
+
+        cells.forEach((e) => {
+            e.innerText = "";
+        });
+
+        currentPlayer = iconX;
+    });
+
+    
+});
